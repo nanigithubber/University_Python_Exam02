3fbfa538f15fffddfb4be4706600a301f68f4501
nanigithubber <rachanacornelia@gmail.com>
2024-07-01 15:14:15 +0200

    Add files via upload

--- exam_2.py
+++ exam_2.py
@@ -25,37 +25,49 @@ def write_file(filename: str, data: list, keep_even: bool):
 
 def filter_numbers(data: list, keep_even: bool): 
     if keep_even == True:
-       return list(filter(lambda x: x % 2 == 0, data))
+        return list(filter(lambda x: x % 2 == 0, data))
     else:
         return list(filter(lambda x: x % 2 != 0, data))
 
-def print_results():
-    percentages = float()
-    
-    overall_percent = 100
-    percentages += (overall_percent,)
+def calc_percentages():
 
-    odd_percent = 100
-    percentages += (odd_percent,)
+    test_even_list = filter_numbers(test_values, True)
+    test_odd_list = filter_numbers(test_values, False)
 
-    even_percent = 100
-    percentages += (even_percent,)
+    correct_even_list = filter_numbers(correct_values, True)
+    correct_odd_list = filter_numbers(correct_values, False)
 
-    print(f'Percentage of numbers remembered:  {percentages[0]:2f}%')
-    print(f'  --> odd numbers remembered    :  {percentages[1]:2f}%')
-    print(f'  --> even numbers remembered   :  {percentages[2]:2f}%')
+    overall_percent = (len(test_values)) / (len(correct_values)) * 100
+    odd_percent = (len(test_odd_list) / len(correct_odd_list)) * 100
+    even_percent = (len(test_even_list) / len(correct_even_list)) * 100
+    percentages = (overall_percent, odd_percent, even_percent)
+
+    return percentages
+
+def print_results(percentages: tuple):     
+    print(f'Percentage of numbers remembered:  {percentages[0]:.2f}%')
+    print(f'  --> odd numbers remembered    :  {percentages[1]:.2f}%')
+    print(f'  --> even numbers remembered   :  {percentages[2]:.2f}%')
 
 def main():
     input_correct = input('Enter the path to the following files:\nCORRECT DATA : ')
-    input_test= input('TEST DATA    : ')
+    input_test = input('TEST DATA    : ')
+    
+    global correct_values
+    correct_values = read_file(input_correct) # save list with correct values
+    global test_values
+    test_values = read_file(input_test) # saves list with test data
 
-    absolute_correct = read_file(input_correct)
-    absolute_test = read_file(input_test)
+    name_test_even = 'output_even.txt'
+    name_test_odd = 'output_odd.txt'
 
-    write_file('output_even.txt', absolute_test, True) #even
-    write_file('output_odd.txt', absolute_test, False) #odd
+    write_file(name_test_even, test_values, keep_even = True)
+    write_file(name_test_odd, test_values, keep_even = False)
 
-    # print_results()
+    percentages = calc_percentages()
+
+    print_results(percentages)
+    
 
 if __name__ == '__main__':
     main()
